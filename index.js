e77e45a210c08c070fa6acc39becf36641ea1f9f
Warhol889 <76567446+Warhol889@users.noreply.github.com>
2020-12-25 02:47:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 const { YTSearcher } = require('ytsearcher');
 
 const searcher = new YTSearcher({
-    key: "YOUTUBE_API",
+    key: "AIzaSyDv2gPwT2tqSkyClxImy_xEKxjdYlx7n5M",
     revealed: true
 });
 
@@ -110,4 +110,4 @@ client.on("message", async(message) => {
     }
 )
 
-client.login('process.env.TOKEN');
+client.login(process.env.token);
