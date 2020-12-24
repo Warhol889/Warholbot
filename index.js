cd04a621a8cdb90ec47b1ad1e880aab593b05d01
unknown <icantusemymailwithoutphonegg@gmail.com>
2020-12-25 01:12:18 +0700

    Added processes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 const { YTSearcher } = require('ytsearcher');
 
 const searcher = new YTSearcher({
-    key: "process.env.YOUTUBE_API",
+    key: process.env.youtube_api,
     revealed: true
 });
 
@@ -110,4 +110,4 @@ client.on("message", async(message) => {
     }
 )
 
-client.login("process.env.TOKEN")
+client.login("process.env.token")
